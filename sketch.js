bf544a361ece6142c2d3ee290b57480bfdf985db
chrischne <chrischne@gmail.com>
2019-09-26 21:58:31 +0200

    initial

--- sketch.js
+++ sketch.js
@@ -2,17 +2,40 @@
 
 
 function setup () {
-	createCanvas(4 * 800, 600);
+	createCanvas(4 * 1920, 1080);
+
+	//HSB mode Hue, Saturation, Brightness
+	colorMode(HSB);
 
 	frameRate(30);
-
+	noLoop();
 }
 
 function draw () {
 
-	background(200);
+	background(0);
+	randomSeed(0);
 
+	for(var i=0; i<100; i++){
+		var radius = random(3,200);
+		var x = random(0,width);
+		var y = random(0,height);
+		var h = random(100,255);
+		var s = random(100,255);
+		var b = map(radius,1,200,255,0);
 
+		noStroke();
+		fill(h,s,b);
+		ellipse(x,y,radius,radius);
+
+	}
+
+}
+
+function keyTyped(){
+	console.log('saving canvas');
+	saveCanvas('myCanvas');
+	console.log('done');
 }
 
 
