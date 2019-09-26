b8f8c9802f6e43fadd0d994ba521f07186a5e1c7
chrischne <chrischne@gmail.com>
2019-09-26 22:05:33 +0200

    format document

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
-
 function setup () {
 	createCanvas(4 * 1920, 1080);
 
@@ -16,23 +15,23 @@ function draw () {
 	background(0);
 	randomSeed(0);
 
-	for(var i=0; i<100; i++){
-		var radius = random(3,200);
-		var x = random(0,width);
-		var y = random(0,height);
-		var h = random(100,255);
-		var s = random(100,255);
-		var b = map(radius,1,200,255,0);
+	for (var i = 0; i < 100; i++) {
+		var radius = random(3, 200);
+		var x = random(0, width);
+		var y = random(0, height);
+		var h = random(100, 255);
+		var s = random(100, 255);
+		var b = map(radius, 1, 200, 255, 0);
 
 		noStroke();
-		fill(h,s,b);
-		ellipse(x,y,radius,radius);
+		fill(h, s, b);
+		ellipse(x, y, radius, radius);
 
 	}
 
 }
 
-function keyTyped(){
+function keyTyped () {
 	console.log('saving canvas');
 	saveCanvas('myCanvas');
 	console.log('done');
